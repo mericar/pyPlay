34fafb913c558d4361a29746fa0154d95e74587b
root <root@mirror8.localdomain>
2016-10-29 02:59:27 +0000

    you've got mail ...

---
+++ tools/postal.py
@@ -0,0 +1,26 @@
+import smtplib
+
+def prompt(prompt):
+    return input(prompt).strip()
+
+from_addr = prompt("From: ")
+to_addr  = prompt("To: ").split()
+print("Enter the message, terminating with ^D ")
+
+message = ("From: %s\r\nTo: %s\r\n\r\n"
+       % (from_addr, ", ".join(to_addr)))
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    if not line:
+        break
+    message = message + line
+
+print("The message length => ", len(message))
+
+mail_server = smtplib.SMTP('localhost')
+mail_server.set_debuglevel(1)
+mail_server.sendmail(from_addr, to_addr, message)
+mail_server.quit()
