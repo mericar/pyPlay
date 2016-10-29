b7bb39d41ed768e7ca12d7e93ff32bfaa4ffcba2
mericar <mirkocarich@gmail.com>
2016-10-28 20:18:22 -0700

    additional comments on postal.

--- tools/postal.py
+++ tools/postal.py
@@ -1,14 +1,17 @@
 import smtplib
 
+#get commandline args:
 def prompt(prompt):
     return input(prompt).strip()
 
+#set the to/from addresses:
 from_addr = prompt("From: ")
 to_addr  = prompt("To: ").split()
 print("Enter the message, terminating with ^D ")
 
 message = ("From: %s\r\nTo: %s\r\n\r\n"
        % (from_addr, ", ".join(to_addr)))
+
 while True:
     try:
         line = input()
@@ -20,6 +23,7 @@ while True:
 
 print("The message length => ", len(message))
 
+#serves the mail from localhost, terminates the server.
 mail_server = smtplib.SMTP('localhost')
 mail_server.set_debuglevel(1)
 mail_server.sendmail(from_addr, to_addr, message)
